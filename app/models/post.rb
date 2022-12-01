8bb8c112aaac21ab1f7e057af9672090c58db0b2
yasinabdmahmood <abd181103@gmail.com>
2022-12-02 01:00:31 +0300

    Add validation for Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,11 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
-
+  
+  validates :Title, presence: true, length: { in: 1..250 }
+  validates :CommnetsCounter , comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
+  validates :LikesCounter , comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
+  
   def self.update_post_counter(user, new_value)
     Post.find_by(user:).user.update(PostsCounter: new_value)
   end
