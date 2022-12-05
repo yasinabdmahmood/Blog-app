e9776d72d249d4400cefa5daa44619393e8116ec
yasinabdmahmood <abd181103@gmail.com>
2022-12-05 14:54:26 +0300

    Add view for all the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "users#index"
   get '/users/:id' , to: 'users#show'
-  get '/users/:id/posts' , to: 'post#index'
+  get '/users/:id/posts' , to: 'posts#index'
   get '/users/:id/posts/:post_id' , to: 'posts#show'
 end
