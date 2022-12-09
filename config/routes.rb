3c7be62bc5457b278eb7c728cf14ab4cdabdb699
yasinabdmahmood <abd181103@gmail.com>
2022-12-09 16:56:18 +0300

    Add Form to save new post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "users#index"
   get '/users/:id' , to: 'users#show'
-  get '/users/:id/posts' , to: 'posts#index'
+  get '/users/:id/posts' , to: 'posts#index', as: 'posts'
+  get '/users/:id/posts/new', to: 'posts#new', as: 'new_post'
+  post '/users/:id/posts/create', to: 'posts#create', as: 'create_post'  
   get '/users/:id/posts/:post_id' , to: 'posts#show'
 end
