adaf5a9df19c72cd8b32389b624cf661f730d796
bin hussen <mhdhsna@gmail.com>
2022-12-12 20:21:26 +0300

    Test All users list

--- spec/view/user_index_spec.rb
+++ spec/view/user_index_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Hello world', type: :system do
     it 'shows the right content' do
       
       visit users_path
-      expect(page).to have_content('Hello, world!')
+      expect(page).to have_content('Display All the users')
     end
   end
 end
